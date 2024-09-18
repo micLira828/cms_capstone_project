11911a15c549111be9e77a23159d04d8189908a8
micLira828 <micgep1015@gmail.com>
2024-09-18 14:04:25 -0700

    Added error handling

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -21,4 +21,11 @@ router.get("/api/csrf/restore", (req, res) => {
 });
 // ...
 
+// backend/routes/index.js
+// ...
+const apiRouter = require('./api');
+
+router.use('/api', apiRouter);
+// ...
+
 module.exports = router;
