c71992a11bbb8916e0cc645d9cc993d89d82c02a
micLira828 <micgep1015@gmail.com>
2024-09-18 18:01:31 -0700

    Added logout functionality

--- backend/routes/api/session.js
+++ backend/routes/api/session.js
@@ -48,6 +48,20 @@ router.post(
     }
   );
 
-  module.exports = router;
+  // backend/routes/api/session.js
+// ...
 
+
+// Log out
+router.delete(
+    '/',
+    (_req, res) => {
+      res.clearCookie('token');
+      return res.json({ message: 'success' });
+    }
+  );
+  
+  // ...
+
+  module.exports = router;
 
