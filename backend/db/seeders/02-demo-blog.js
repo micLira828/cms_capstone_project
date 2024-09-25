2ef648b0373d0972beed360914e291effcd83fef
micLira828 <micgep1015@gmail.com>
2024-09-24 19:35:03 -0700

    Added seeders

--- backend/db/seeders/02-demo-blog.js
+++ backend/db/seeders/02-demo-blog.js
@@ -11,8 +11,40 @@ module.exports = {
      *   name: 'John Doe',
      *   isBetaMember: false
      * }], {});
+     * 
     */
+    await Blog.bulkCreate([
+      { userId: 1,
+        title: "Demo of Utter Wellness",
+        description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book"
+      },
+      { userId: 1,
+        title: "My Mega Money Management",
+        description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."
+      },
+      {
+        userId: 2,
+        title: "My Thoughts",
+        description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."
+      },
+      {
+        userId: 2,
+        title: "My Fitness Journey",
+        description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."
+      },
+      {
+        userId: 3,
+        title: 'Beautiful Life',
+        description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."
+      },
+      {
+        userId: 3,
+        title: 'Emerging Growth',
+        description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."
+      }
+    ], { validate: true });
   },
+  
 
   async down (queryInterface, Sequelize) {
     /**
--- backend/db/seeders/03-demo-post.js
+++ backend/db/seeders/03-demo-post.js
@@ -12,6 +12,42 @@ module.exports = {
      *   isBetaMember: false
      * }], {});
     */
+    await Post.bulkCreate([
+      { userId: 1,
+        blogId: 1,
+        title: "Why I am so well",
+        postEntry: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book"
+      },
+      { userId: 1,
+        blogId: 2,
+        title: "Multiplying Funds",
+        postEntry: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."
+      },
+      {
+        userId: 2,
+        blogId: 3,
+        title: "3am, cannot sleep! Why?",
+        description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."
+      },
+      {
+        userId: 2,
+        blogId: 4,
+        title: "One Step, Two Step",
+        description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."
+      },
+      {
+        userId: 3,
+        blogId: 5,
+        title: 'Gazing at the Romantic Sunset',
+        description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."
+      },
+      {
+        userId: 3,
+        blogId: 6,
+        title: 'Emerging Growth',
+        description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."
+      }
+    ], { validate: true });
   },
 
   async down (queryInterface, Sequelize) {
