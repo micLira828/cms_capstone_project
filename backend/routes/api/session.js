c14ccc0bc5769caf488be392708c373045b221c9
micLira828 <micgep1015@gmail.com>
2024-09-30 19:05:44 -0700

    Added route

--- backend/routes/api/session.js
+++ backend/routes/api/session.js
@@ -73,6 +73,25 @@ router.post(
 // ...
 
 
+router.get(
+  '/',
+  (req, res) => {
+    const { user } = req;
+    if (user) {
+      const safeUser = {
+        id: user.id,
+        firstName: user.firstName,
+        lastName: user.lastName,
+        email: user.email,
+        username: user.username
+      };
+      return res.json({
+        user: safeUser
+      });
+    } else return res.json({ user: null });
+  }
+);
+
 // Log out
 router.delete(
     '/',
