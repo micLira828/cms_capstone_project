46a4af23dbfa6ed9b3c0a8931fa9499c7fb28dee
micLira828 <micgep1015@gmail.com>
2024-10-01 20:54:47 -0700

    Added login and signup buttons

--- frontend/src/components/Navigation.jsx
+++ frontend/src/components/Navigation.jsx
@@ -1,27 +1,39 @@
-// frontend/src/components/Navigation/Navigation.jsx
-
 import { NavLink } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 import ProfileButton from './ProfileButton';
-import './Navigation.css'
+import OpenModalButton from './OpenModalButton';
+import LoginFormModal from './LoginFormModal';
+import SignupFormModal from './SignupFormModal';
+import './Navigation.css';
 
-function Navigation({ isLoaded }){
-  const sessionUser = useSelector(state => state.session.user);
+function Navigation({ isLoaded }) {
+  const sessionUser = useSelector((state) => state.session.user);
 
-  const sessionLinks = sessionUser ? (
-    <li>
-      <ProfileButton user={sessionUser} />
-    </li>
-  ) : (
-    <>
+  let sessionLinks;
+  if (sessionUser) {
+    sessionLinks = (
       <li>
-        <NavLink to="/login">Log In</NavLink>
+        <ProfileButton user={sessionUser} />
+      </li>
+    );
+  } else {
+    sessionLinks = (
+      <ul>
+      <li>
+        <OpenModalButton
+          buttonText="Log In"
+          modalComponent={<LoginFormModal />}
+        />
       </li>
       <li>
-        <NavLink to="/signup">Sign Up</NavLink>
+        <OpenModalButton
+          buttonText="Sign Up"
+          modalComponent={<SignupFormModal />}
+        />
       </li>
-    </>
-  );
+      </ul>
+    );
+  }
 
   return (
     <ul>
