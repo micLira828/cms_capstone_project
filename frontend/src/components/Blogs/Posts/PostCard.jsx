689a841eb4eb0385af0081da00011207a28472e8
micLira828 <micgep1015@gmail.com>
2024-10-04 16:19:12 -0700

    Added untested full crud for posts

---
+++ frontend/src/components/Blogs/Posts/PostCard.jsx
@@ -0,0 +1,28 @@
+import OpenModalButton from '../../OpenModalButton';
+import { useSelector } from "react-redux";
+import DeletePostModal from "./DeletePostModal";
+import UpdatePostModal from "./UpdatePostModal";
+const PostCard = ({post}) => {
+
+    const sessionUser = useSelector((state) => state.session.user);
+
+    
+    return (<div className = "post">
+    {/* <h4>{review?.User?.firstName}</h4> */}
+    {/* <h4>{verbalDate}</h4> */}
+     <p>{post.postEntry}</p>
+     {sessionUser && sessionUser.id === post.userId ? (<>
+        <OpenModalButton 
+                 modalComponent = {<UpdatePostModal postId = {post.id}/>}
+                 buttonText = {'Update Post'}
+                 />
+     <OpenModalButton 
+                 modalComponent = {<DeletePostModal postId = {post.id}/>}
+                 buttonText = {'Delete Post'}
+                />
+    </>) : ""}
+    </div>)
+   
+}
+
+export default PostCard;
--- frontend/src/components/Blogs/Posts/Posts.jsx
+++ frontend/src/components/Blogs/Posts/Posts.jsx
@@ -1,6 +1,49 @@
 
 
-const Posts = () => ({
-    
-});
+import { useEffect} from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import PostCard from './PostCard';
+import { getBlogPosts } from '../../../store/post';
+
+import OpenModalButton from '../../OpenModalButton';
+// import { useModal } from '../../context/Modal';
+
+// import './Reviews.css';
+import CreatePostModal from './CreatePostModal';
+const Posts = ({blog}) => {
+  const dispatch = useDispatch();
+
+  let sessionUser = useSelector((state) => state.session.user);
+
+
+  let blogPosts = useSelector((state) => state.allPosts);
+  
+  
+  useEffect(() => {
+    dispatch(getBlogPosts({blog}));
+  }, [dispatch, blog])
+
+
+
+  const usersPost = sessionUser ? blogPosts.find(post => {return post.userId === sessionUser.id}) : undefined;
+
+  return (
+    <>
+      <div className = "posts">
+       
+       {sessionUser && blog.userId === sessionUser.id 
+       && !usersPost ? ( <><OpenModalButton className = "modalRedRectangular"
+        modalComponent = {<CreatePostModal blog = {blog}/>}
+        buttonText = {'Create a post'}
+       /></>) 
+       : ""}
+        {blogPosts.map((post) => <PostCard post = {post} key = {post.id}/>)}
+        {blogPosts.length < 1 ? <p>Create your first post!</p> : ""}
+      </div>
+    {/* if user id matches current users if and if there is a user */}
+   
+    </>
+  )
+}
+
 export default Posts;
