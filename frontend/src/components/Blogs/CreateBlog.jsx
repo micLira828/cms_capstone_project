ee5fa3c4e33bf801d58c4540847937d99b92fcc0
micLira828 <micgep1015@gmail.com>
2024-10-08 14:12:31 -0700

    Created the component to create the blog

---
+++ frontend/src/components/Blogs/CreateBlog.jsx
@@ -0,0 +1,52 @@
+import {useState} from 'react';
+import {useDispatch} from 'react-redux';
+import { useNavigate } from 'react-router-dom';
+import { postSpot} from '../../store/spot';
+import { postSpotImage} from '../../store/spotImage';
+
+const CreateBlog = () => {
+    const [title, setTitle] = useState('');
+    const [description, setDescription] = useState('');
+
+    const navigate = useNavigate();
+
+    const dispatch = useDispatch();
+
+    const handleSubmit = async(e) => {
+        e.preventDefault();
+     
+      
+
+        const form = {
+           title,
+           postEntry
+        }
+        
+        const newBlog = await dispatch(postBlog(form));
+       
+
+        navigate(`/blogs/current`);
+    }
+
+  
+    return (
+        <>
+         <h2>Create a new blog</h2>
+        <form onSubmit={handleSubmit}>
+            <h3>Add a snazzy title for your Genius Masterpiece</h3>
+          
+            <input placeholder = "Title of your Blog" onChange={(e) => setTitle(e.target.value)} value = {title}  type = "text"></input>
+                <h3>Describe your blog to guests</h3>
+                <p>Provide a written sneak-preview/intro of the best features of your blog
+                    and the topics and subjects that the blog covers that readers will want to
+                    know more about, to hook them in 
+                </p>
+                <textarea placeholder = "Please write at least 30 characters" className = "description" onChange={(e) => setDescription(e.target.value)} value = {description}  rows = "10" cols = "20"></textarea>
+                  
+            <button className = "redRectangular" type = 'submit'>Create your Blog</button>
+        </form>
+        </>
+    )
+}
+
+export default CreateBlog;
