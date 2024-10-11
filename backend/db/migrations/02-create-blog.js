19b98eb115360fd1f152999356c8065c56190750
micLira828 <micgep1015@gmail.com>
2024-10-10 19:24:50 -0700

    Added options object to migration files

--- backend/db/migrations/02-create-blog.js
+++ backend/db/migrations/02-create-blog.js
@@ -1,5 +1,6 @@
 'use strict';
 /** @type {import('sequelize-cli').Migration} */
+let options = {};
 module.exports = {
   async up(queryInterface, Sequelize) {
     await queryInterface.createTable('Blogs', {
